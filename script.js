d0a2a365d98b91080eb505f4887522fb9c6a7f7a
adeniranadeyemo <niranadeniran1@gmail.com>
2023-07-31 09:26:48 +0100

    Made the JavaScript more understandable

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+const woodenCon = document.getElementById('wooden');
 const slide = document.getElementById('slide');
 const upArrow = document.getElementById('upArrow');
 const downArrow = document.getElementById('downArrow');
 
+const fontSizePixels = parseFloat(window.getComputedStyle(slide).fontSize);
+
+const woodenHeightInPixels = woodenCon.offsetHeight;
+const woodenHeightInEm = woodenHeightInPixels / fontSizePixels;
+
+const slideHeightInPixels = slide.offsetHeight;
+const slideHeightInEm = slideHeightInPixels / fontSizePixels;
+
+const neededHeight = slideHeightInEm - woodenHeightInEm;
+
 let x = 0;
 
-const moveUp = function () {
-  if (x > '-46') {
-    x -= 23;
+const moveSlideUp = function () {
+  if (x > `-${neededHeight}`) {
+    x -= woodenHeightInEm;
     slide.style.top = x + 'em';
   }
 };
 
-const moveDown = function () {
+const moveSlideDown = function () {
   if (x < 0) {
-    x += 23;
+    x += woodenHeightInEm;
     slide.style.top = x + 'em';
   }
 };
 
-upArrow.addEventListener('click', moveUp);
-downArrow.addEventListener('click', moveDown);
+upArrow.addEventListener('click', moveSlideDown);
+downArrow.addEventListener('click', moveSlideUp);

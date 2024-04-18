865ffbd7ad63502ef72ecb61e9700fda5fca3568
adeniranadeyemo <niranadeniran1@gmail.com>
2024-04-18 10:37:07 +0100

    Update to scrolling.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
 const woodenCon = document.getElementById('wooden');
 const slide = document.getElementById('slide');
 const upArrow = document.getElementById('upArrow');
@@ -13,6 +15,26 @@ const slideHeightInEm = slideHeightInPixels / fontSizePixels;
 
 const neededHeight = slideHeightInEm - woodenHeightInEm;
 
+// Scrolling to section
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Navigate/scroll to specific spot
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 75;
+
+    if (id === 'contact') {
+      position = position + 80;
+    }
+
+    window.scrollTo(0, position);
+  });
+});
+
+// Scrolling showroom
 let x = 0;
 
 const moveSlideUp = function () {
